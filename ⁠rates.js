ad406dbccbd64df3660cb0d4cc93661238e34afb
tarekcbe-a11y <tarekcbe@gmail.com>
2026-08-16 02:49:38 +0300

    Update ⁠rates.js

--- ⁠rates.js
+++ ⁠rates.js
@@ -1,11 +1,73 @@
-window.currencyData = {
-    rates: {
-        "EGP": { name: "🇪🇬 جنيه مصري", sell: 1.00 },
-        "USD": { name: "🇺🇸 دولار أمريكي", sell: 50.28 },
-        "EUR": { name: "🇪🇺 يورو", sell: 58.00 },
-        "GBP": { name: "🇬🇧 جنيه إسترليني", sell: 67.82 },
-        "SAR": { name: "🇸🇦 ريال سعودي", sell: 13.38 },
-        "AED": { name: "🇦🇪 درهم إماراتي", sell: 13.68 },
-        "KWD": { name: "🇰🇼 دينار كويتي", sell: 163.75 }
+// ملف متجدد يومياً بأسعار العملات (الدولار في المقدمة)
+const exchangeRates = [
+    {
+        code: "USD",
+        name: "دولار أمريكي",
+        flag: "🇺🇸",
+        buy: "50.23",
+        sell: "50.33"
+    },
+    {
+        code: "EUR",
+        name: "يورو",
+        flag: "🇪🇺",
+        buy: "54.90",
+        sell: "55.10"
+    },
+    {
+        code: "SAR",
+        name: "ريال سعودي",
+        flag: "🇸🇦",
+        buy: "13.34",
+        sell: "13.40"
+    },
+    {
+        code: "GBP",
+        name: "جنيه إسترليني",
+        flag: "🇬🇧",
+        buy: "64.73",
+        sell: "64.91"
+    },
+    {
+        code: "AED",
+        name: "درهم إماراتي",
+        flag: "🇦🇪",
+        buy: "13.66",
+        sell: "13.70"
+    },
+    {
+        code: "CHF",
+        name: "فرنك سويسري",
+        flag: "🇨🇭",
+        buy: "58.66",
+        sell: "58.88"
+    },
+    {
+        code: "BHD",
+        name: "دينار بحريني",
+        flag: "🇧🇭",
+        buy: "131.30",
+        sell: "133.00"
+    },
+    {
+        code: "KWD",
+        name: "دينار كويتي",
+        flag: "🇰🇼",
+        buy: "163.20",
+        sell: "164.50"
+    },
+    {
+        code: "QAR",
+        name: "ريال قطري",
+        flag: "🇶🇦",
+        buy: "13.75",
+        sell: "13.82"
+    },
+    {
+        code: "CNY",
+        name: "يوان صيني",
+        flag: "🇨🇳",
+        buy: "7.04",
+        sell: "7.10"
     }
-};
+];
